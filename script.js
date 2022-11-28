3ccedac4123b0967dd248d6092ce3ea46bd27f5a
dbochner <dbochner@schools.nyc.gov>
2022-11-28 11:34:30 -0500

    Added js-section and js events

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let theme = document.getElementByID('About-Me-content');
-let togButton = document.getElementById('toggleButton');
+let itemOne = document.getElementById('toggle-button');
+let itemTwo = document.getElementById('dateString');
 
+function modifyContent() {
+    itemOne.innerHTML = 'I should be hidden';
+    itemOne.style.color = 'white';
+    itemOne.style.backgroundColor = 'black';
+    itemOne.style.display = 'none';
+    itemTwo.style.display = 'block';
+    itemTwo.style.margin = '0.5rem auto'; 
+};
 
-function changeColor() {
-    togButton.style.color = 'white';
-    togButton.style.backgroundColor = 'black';
+itemOne.addEventListener('mousedown', modifyContent);
+
+function changeText() {
+    itemOne.innerHTML = 'Click Me!!!';
 }
 
-togButton.addEventListener('mouseover', changeColor);
+itemOne.addEventListener('mouseover', changeText);
+
+function revertText() {
+    itemOne.innerHTML = 'Hover over me for a surprise';
+}
+
+itemOne.addEventListener('mouseout', revertText);
+
+
+const d = new Date();
+let text = d.toLocaleDateString();
+document.getElementById("dateString").innerHTML = `Today's Date is ${text}`;
